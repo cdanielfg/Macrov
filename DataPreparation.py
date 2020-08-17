a3d6f94e4343929715860380cbe66d52b3d1fc10
BerserkBlaze <49006400+BerserkBlaze@users.noreply.github.com>
2020-08-17 17:40:59 -0500

    data preparation and operations

--- DataPreparation.py
+++ DataPreparation.py
@@ -14,3 +14,18 @@ import matplotlib.pyplot as plt
 data = pd.read_excel("MACRO.xlsx", shet_name=0)
 
 data=data.fillna(0)
+presupuesto=0
+lista = ['1.1.1','2.2.2','2.2.3']
+cantidad=[1,2,3]
+
+filtro = data[data['No. Item'].isin(lista)]
+precios=filtro['Precio Unitario'].tolist()
+
+for i in range(len(cantidad)):
+    presupuesto+=cantidad[i]*precios[i]
+    
+print(presupuesto)
+
+
+    
+
